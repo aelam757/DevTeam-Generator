177968fe7c34a619d5f4c89272bcc33376c77519
Alan Elam <aelam757@gmail.com>
2020-02-24 21:54:48 -0500

    working on team member functions

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const employees = [];
+
+let teamMembers = {
+    Manager: null,  
+    Engineers: [],
+    Intern: []
+}
 
 function init(){
     startHTML();
-    addMembers();
+    teamMember();
 }
+
 // Team Member Prompts
-function addMembers(){
+function teamMember(){
     inquirer
     .prompt([{
+        type: "list",
+        name: "selectOption",
+        message: "What is the role you are adding to the team?",
+        choices: ["Add a manager", "Add an engineer", "Add an intern", "Quit"]
+    },
+    {
         type: "input",
         name: "name",
         message: "Please enter team member's name:"
     },
     {
         type: "list",
-        name: "name",
+        name: "role",
         message: "Select title for member:",
         choices: ["Intern", "Engineer", "Manager"]
     },
@@ -34,5 +46,116 @@ function addMembers(){
         message: "Please enter team member's Id number:"
     }])
 
+    // Response to adding a team member.
+    .then(response => {
+        switch (response.selectedOption) {
+          case "Add a manager":
+            console.log("Adding the manager!");
+            if (teamMembers.manager === null) {
+              addManager();
+            } else {
+              console.log("There can only be one manager at a time!");
+              promptUser();
+            }
+            break;
+          case "Add a engineer":
+            console.log("Adding a engineer!");
+                addEngineer();
+            break;
+          case "Add a intern":
+              console.log("Adding a intern!");
+                addIntern();
+            break;
+        }
+    });
 }
+            // Manager Function 
+            function Manager() {
+                console.log("Lets add a manager");
+                inquirer.prompt([
+                  {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is the manager's name?"
+                  },
+                  {
+                    type: "input",
+                    name: "managerId",
+                    message: "What is the manager's id?"
+                  },
+                  {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is the manager's email?"
+                  },
+                  {
+                    type: "input",
+                    name: "officeNumber",
+                    message: "What is the manager's office number?"
+                  }])
+
+                  .then (response => {
+
+                  })
+            }
+                
+                // Engineer Function
+                function Engineer() {
+                    console.log("Lets add a engineer!");
+                    inquirer.prompt([{
+                        type: "input",
+                        name: "engineerName",
+                        message: "What is the engineer's name?"
+                      },
+                      {
+                        type: "input",
+                        name: "engineerId",
+                        message: "What is the engineer's id?"
+                      },
+                      {
+                        type: "input",
+                        name: "engineerEmail",
+                        message: "What is the manager's email?"
+                      },
+                      {
+                        type: "input",
+                        name: "engineerGithub",
+                        message: "What is the engineer's Github?"
+                      }])
+
+                      .then(response => {
+
+                  })
+            }
+
+                // Intern Function
+                function Intern() {
+                    console.log("Lets add a intern!");
+                    inquirer.prompt([{
+                        type: "input",
+                        name: "internName",
+                        message: "What is the intern's name?"
+                      },
+                      {
+                        type: "input",
+                        name: "internId",
+                        message: "What is the intern's id?"
+                      },
+                      {
+                        type: "input",
+                        name: "internEmail",
+                        message: "What is the intern's email?"
+                      },
+                      {
+                        type: "input",
+                        name: "internSchool",
+                        message: "Where did they go to school?"
+                      }])
+                      
+                      .then(response => {
+
+                  })
+            }
+
+                
 init();
